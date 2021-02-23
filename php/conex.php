90b8f70585a35d4f571cd6fc902af8c51c08ad01
DavidAlejo57 <davidacaice@hotmail.com>
2021-02-22 21:13:25 -0500

    version 5

--- php/conex.php
+++ php/conex.php
@@ -1,8 +1,8 @@
 <?php
-	$usuario='root';
-	$clave ='';
-	$servidor='localhost';
-	$base = 'baseempresa';
+	$usuario='u8qfmagpk7gaii9q';
+	$clave ='ADhO50Z8UKRpVDNTs1yA';
+	$servidor='b5dev4ncmojtxhabtc52-mysql.services.clever-cloud.com';
+	$base = 'b5dev4ncmojtxhabtc52';
 	$l=@mysqli_connect($servidor,$usuario,$clave) or die("Error al conectarse con el servidor");
 	@mysqli_select_db($l,$base) or die("Error al conectarse a la bd");
 	@mysqli_set_charset($l, 'utf8');
